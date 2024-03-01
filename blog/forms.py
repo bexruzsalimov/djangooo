ceb680f221fcde7d9a1afda992d337b32c6dafe0
bexruzsalimov <ergachevulmas76@gmail.com>
2024-03-01 17:15:02 +0300

    pushme

---
+++ blog/forms.py
@@ -0,0 +1,33 @@
+from user.models import User
+from django import forms
+from .models import Post, Category
+
+      
+
+
+
+class CreatePostForm(forms.Form):
+    category = forms.ChoiceField(
+        choices=[(category.id, category.name)
+                 for category in Category.objects.all()],
+                 required=True,
+                 widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Title', 'type': 'text'}
+    ))
+    summary = forms.CharField(max_length=255, required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Summary', 'type': 'text'}
+    ))
+    text = forms.CharField(
+        widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'Body', 'rows': '3'}
+    ))
+    image = forms.ImageField(
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['category', 'name', 'summary', 'text', 'image']
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post, Category
-
+from blog.forms import CreatePostForm
 
 def home(request):
     posts = Post.objects.all()
@@ -26,8 +26,27 @@ def post_detail(request, id):
 
 def post_create(request):
 
+    if request.method == 'POST':
+        form = CreatePostForm(request.POST, request.FILES)
+        user = request.user
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            image = form.cleaned_data['image']
+            category_id = form.cleaned_data['category']
+            category = Category.objects.get(pk=category_id)
+
+            post = Post.objects.create(
+                author=user,
+                text=text,
+                img=image,
+                category=category,
+            )
+            return redirect('user:dashboard', id=user.id)
+    else:
+        form = CreatePostForm()
+
     data = {
-        
+        'form' :form,
     }
 
     return render(request, 'posts/post-create.html', data)
