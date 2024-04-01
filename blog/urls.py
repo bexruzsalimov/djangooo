f30201058f7ef4e59a0455a27851d371b2d7d69f
bexruzsalimov <ergachevulmas76@gmail.com>
2024-04-01 14:34:37 +0300

    B.S.A
    B.S.A

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 app_name = "blog"
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home', views.home, name="home"),
     path('search/<str:query>/', views.search, name="search"),
     path('post-detail/<int:id>/', views.post_detail, name='post_detail' ),
     path('post-create/', views.post_create, name="post_create"),
