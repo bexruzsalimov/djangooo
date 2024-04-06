3b4b248cc10a150faf0950d3ba893eaa99373ef7
bexruzsalimov <ergachevulmas76@gmail.com>
2024-04-06 20:39:04 +0300

    Update views.py

--- user/views.py
+++ user/views.py
@@ -50,7 +50,7 @@ def profile(request, id):
 
     if request.method == "POST":
         full_name = request.POST.get('full_name')
-        avatar = request.FILES.get('avatar')
+        avatar = request.FILE.get('avatar')
         phone = request.POST.get('phone')
         job = request.POST.get('job')
         bio = request.POST.get('bio')
